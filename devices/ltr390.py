5b34c4cc6673f626872cdb75e8085a88451e76c9
keithyoder <yoder@tessi.com.br>
2026-09-08 10:11:55 -0300

    reduce ltr390 resolution

--- devices/ltr390.py
+++ devices/ltr390.py
@@ -12,7 +12,7 @@ class LTR390(Device):
             from board import I2C
             self.device = adafruit_ltr390.LTR390(I2C())
             self.device.gain = adafruit_ltr390.Gain.GAIN_1X
-            self.device.resolution = adafruit_ltr390.Resolution.RESOLUTION_16BIT
+            self.device.resolution = adafruit_ltr390.Resolution.RESOLUTION_13BIT
         except:
             self.device = None
         self.values = {}
